9837430002b224e0f986baad89c7dddff64bede7
arpitpatel33 <arpit.patel33@gmail.com>
2023-02-14 10:34:39 +0530

    Accomodated PR comment changes

--- jpaNew/src/main/java/com/example/jpaNew/controllers/InvestmentController.java
+++ jpaNew/src/main/java/com/example/jpaNew/controllers/InvestmentController.java
@@ -3,6 +3,8 @@ package com.example.jpaNew.controllers;
 import com.example.jpaNew.entities.Investment;
 import com.example.jpaNew.repositories.InvestmentRepository;
 import com.example.jpaNew.services.InvestmentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -21,9 +23,13 @@ public class InvestmentController {
     @Autowired
     private InvestmentRepository investmentRepository;
 
+    private static final Logger log = LoggerFactory.getLogger(InvestmentController.class);
+
 
     @RequestMapping(method = RequestMethod.GET, value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> all(){
+        final String methodName= "getAll() : ";
+        log.info(methodName + "called");
 
         List<Investment> getAll= investmentService.findAll();
         return new ResponseEntity<>(getAll, HttpStatus.OK);
@@ -32,6 +38,9 @@ public class InvestmentController {
     @RequestMapping(method = RequestMethod.GET,value = "/id/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getById(@PathVariable ("id") Long id)
     {
+        final String methodName= "getById() :";
+        log.info(methodName + "called");
+
         Optional<Investment> byId= investmentService.findById(id);
         return new ResponseEntity<>(byId,HttpStatus.OK);
     }
@@ -39,6 +48,9 @@ public class InvestmentController {
     @RequestMapping(method = RequestMethod.GET,value = "/filter",produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> getByFilter(@RequestParam ("startId") Long startId, @RequestParam ("endId") Long endId)
     {
+        final String methodName= "getByFilter() : ";
+        log.info(methodName + "called");
+
         List<Investment> byFilter= investmentService.getByIdGreaterThanEqualAndIdLessThanEqual(startId,endId);
         return new ResponseEntity<>(byFilter,HttpStatus.OK);
     }
@@ -46,6 +58,9 @@ public class InvestmentController {
     @RequestMapping(method = RequestMethod.POST,value = "/create", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> createById(@RequestBody Investment investment)
     {
+        final String methodName= "createById() : ";
+        log.info(methodName + "called");
+
         Investment byId= investmentService.save(investment);
         return new ResponseEntity<>(byId, HttpStatus.CREATED);
     }
@@ -63,6 +78,9 @@ public class InvestmentController {
     @RequestMapping(method = RequestMethod.DELETE, value = "/delete/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> deleteById(@PathVariable ("id") Long id)
     {
+        final String methodName = "deleteById() : ";
+        log.info(methodName + "called");
+
         investmentService.deleteById(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
@@ -73,6 +91,9 @@ public class InvestmentController {
     @RequestMapping(method = RequestMethod.PUT, value = "/update/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> updateById(@RequestBody Investment investment, @PathVariable ("id") Long id)
     {
+     final String methodName = "updateById() : " ;
+     log.info(methodName + "called");
+
      Investment byId= investmentService.getById(id);
      byId.setId(investment.getId());
      Investment updated= investmentService.save(investment);
